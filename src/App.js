156ad818db4f596c8151c7e9ab9ea8221fd7a8ef
Kevin Chu <kevinchu1337@gmail.com>
2021-02-23 10:07:12 -0500

    refactor for code clarity

--- src/App.js
+++ src/App.js
@@ -4,43 +4,43 @@ import WeatherCard from "./WeatherCard";
 import "./styles/App.css";
 import data from './data/test-data.json';
 import {WEATHER_TYPE_MAP} from './constants'
-import { isValidWeatherDataSet } from './helpers'
+import {isValidWeatherDataSet} from './helpers'
 
 const App = () => {
-  const [startDate, changeStartDate] = useState(new Date(new Date()- 1000 * 60 * 60 * 24 * 1));
+  const [startDate, changeStartDate] = useState(new Date(new Date() - 1000 * 60 * 60 * 24 * 1));//Set to yesterday
   const [endDate, changeEndDate] = useState(new Date());
   const [location, changeLocation] = useState('');
-  const [weatherData, setWeatherData] = useState(new Map());
+  const [weatherDataMap, setWeatherDataMap] = useState(new Map());
   const [filteredWeatherArray, setFilteredWeatherArray] = useState([]);
 
   useEffect(() => {
+    //Creates the weatherDataMap in the form
+    //{town1: [{weatherSet1}, {weatherSet2}], town2: [{weatherSet1}, {weatherSet2}]}
     const townMap = new Map();
     data.filter(isValidWeatherDataSet).forEach(({date, town, weather}) => {
       const dateObj = new Date(date);
+      const weatherDataSet = {date: dateObj, formattedDate: date, weather: WEATHER_TYPE_MAP[weather]};
       if (!townMap.has(town)) {
-        townMap.set(
-          town, [{date: dateObj, formattedDate: date, weather: WEATHER_TYPE_MAP[weather]}]
-        );
+        townMap.set(town, [weatherDataSet]);
       } else {
-        townMap.get(town).push(
-          {date: dateObj, formattedDate: date, weather: WEATHER_TYPE_MAP[weather]}
-        );
+        townMap.get(town).push(weatherDataSet);
       }
     });
 
-    setWeatherData(townMap);
+    setWeatherDataMap(townMap);
   }, []);
 
   useEffect(() => {
-     if(!weatherData.has(location)){
-       setFilteredWeatherArray([]);
-     }else{
-       const weatherArray = weatherData.get(location).filter(({date}) => {
-         return date <= endDate && date >= startDate;
-       });
-       setFilteredWeatherArray(weatherArray);
-     }
-  }, [startDate, endDate, location, weatherData]);
+    //sets the list of weather data filtered by location and the date range
+    if (!weatherDataMap.has(location)) {
+      setFilteredWeatherArray([]);
+    } else {
+      const weatherArray = weatherDataMap.get(location).filter(({date}) => {
+        return date <= endDate && date >= startDate;
+      });
+      setFilteredWeatherArray(weatherArray);
+    }
+  }, [startDate, endDate, location, weatherDataMap]);
 
   const changeLocationMemoized = useCallback(
     (e) => {
